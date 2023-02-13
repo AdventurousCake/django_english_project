9a538d1157660c08a05f29d8d67a7aab9ab8f225
i <tropitzyn.igor1@gmail.com>
2023-02-13 17:01:58 +0500

    upd

--- stripe_service/models.py
+++ stripe_service/models.py
@@ -36,21 +36,24 @@ class Order(models.Model):
 
     def get_discount_total_cost(self):
         # x / 100 * (100-discount)
-        pass
-        # return self.get_total_cost()/100 * (self.discount.first())
+        return self.get_total_cost() / 100 * (100 - self.get_disc())
 
     def get_disc(self):
         # return self.discount_set.all()
-        return self.discount_set.first().value
+        if self.discount_set:
+            return self.discount_set.first().value
+        else:
+            return 0
 
     def __str__(self):
-        return f"total {self.get_total_cost()}; disc: {self.get_disc()}"
+        return f"total {self.get_total_cost()}; total-disc: {self.get_discount_total_cost()} ; disc: {self.get_disc()}"
 
 
 # from stripe_service.models import Order2, Item
 #  Order2.objects.get(pk=1).discount2_set.first().value
 # todo https://metanit.com/python/django/5.7.php
 
+# множество заказов и множество скидок
 class Discount(models.Model):
     id = models.BigAutoField(primary_key=True, auto_created=True, null=False)
     orders = models.ManyToManyField(to=Order, related_name='discount_set')  # related_name='discount')  # symmetrical?
@@ -62,6 +65,7 @@ class Discount(models.Model):
         # unique?
 
 
+# TEST
 class Order2(models.Model):
     items = models.ManyToManyField(Item)
 
