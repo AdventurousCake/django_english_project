4520db4bfed9823c0550c03e392c5156ed3d72f4
Igor <tropitzyn.igor1@gmail.com>
2025-01-24 15:26:40 +0500

    upd

--- test_settings.py
+++ test_settings.py
@@ -1,5 +1,3 @@
-from urllib.parse import urlparse
-
 from django_test1.settings import *
 
 DEBUG = True
@@ -7,25 +5,37 @@ DEBUG = True
 INSTALLED_APPS.remove('debug_toolbar')
 # MIDDLEWARE.insert(0, 'core.middleware.simple_ip_check')
 
-TEST_DB_URL=os.getenv('TEST_DB_URL')
-if TEST_DB_URL:
-    result = urlparse(TEST_DB_URL)
-    username = result.username
-    password = result.password
-    database = result.path[1:]
-    hostname = result.hostname
-    port = result.port or '5432'
-
 DATABASES = {
-    'default': {
-        'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.postgresql'),
-        'NAME': database,
-        'USER': username,
-        'PASSWORD': password,
-        'HOST': hostname,
-        'PORT': port,
-    }
+    'default':
+        {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'postgres',
+            'USER': 'postgres',
+            'PASSWORD': 'postgres',
+            'HOST': 'localhost',
+            'PORT': '5432',
+        }
 }
+
+# TEST_DB_URL=os.getenv('TEST_DB_URL')
+# if TEST_DB_URL:
+#     result = urlparse(TEST_DB_URL)
+#     username = result.username
+#     password = result.password
+#     database = result.path[1:]
+#     hostname = result.hostname
+#     port = result.port or '5432'
+#
+# DATABASES = {
+#     'default': {
+#         'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.postgresql'),
+#         'NAME': database,
+#         'USER': username,
+#         'PASSWORD': password,
+#         'HOST': hostname,
+#         'PORT': port,
+#     }
+# }
 
 # CACHES = {
 #     "default": {
