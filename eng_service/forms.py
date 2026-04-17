e5636fcc7f1523d69023ef967d5757f710cddb7b
Igor <tropitzyn.igor1@gmail.com>
2026-04-17 14:36:41 +0500

    clean

--- eng_service/forms.py
+++ eng_service/forms.py
@@ -1,15 +1,16 @@
 from django import forms
-from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
 
 from eng_service.models import EngFixer
 
 
 class EngFixerForm(forms.ModelForm):
+    input_sentence = forms.CharField(max_length=256, widget=forms.Textarea(attrs={'rows': 2,
+                                                                                  'autofocus': True}))
+
     class Meta:
         model = EngFixer
         fields = ('input_sentence', 'fixed_sentence')
-        # exclude = ('id',)
 
         error_messages = {
             'input_sentence': {
@@ -17,8 +18,6 @@ class EngFixerForm(forms.ModelForm):
             },
         }
 
-    input_sentence = forms.CharField(max_length=256, widget=forms.Textarea(attrs={'rows': 2}))
-    # fixed_sentence = forms.CharField(widget=forms.Textarea(attrs={'rows': 2}))
 
     # def clean_input_sentence(self):
     #     data = self.cleaned_data['input_sentence']
